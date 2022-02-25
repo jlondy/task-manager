d25bceb42f83fb9fbf9a957ac135667e0cc2e052
Jason Lalonde <jasonlalonde@Jasons-Air.hitronhub.home>
2022-02-25 17:31:56 -0400

    angular tutorial

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,45 @@
+import { Task } from './Task';
+export const TASKS: Task[] = [
+	{
+		id: 1,
+		text: 'hey',
+		day: 'monday',
+		reminder: false
+	},
+	{
+		id: 2,
+		text: 'hey',
+		day: 'monday',
+		reminder: false
+	},
+	{
+		id: 3,
+		text: 'hey',
+		day: 'monday',
+		reminder: false
+	},
+	{
+		id: 4,
+		text: 'hey',
+		day: 'monday',
+		reminder: false
+	},
+	{
+		id: 5,
+		text: 'hey',
+		day: 'monday',
+		reminder: false
+	},
+	{
+		id: 6,
+		text: 'hey',
+		day: 'monday',
+		reminder: false
+	},
+	{
+		id: 7,
+		text: 'hey',
+		day: 'monday',
+		reminder: false
+	}
+];
